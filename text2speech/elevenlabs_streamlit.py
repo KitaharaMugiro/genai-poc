ad331c07d166f5cd95b7607cda429006d28e24b7
KitaharaMugiro <you@example.com>
2023-09-10 14:30:50 +0900

    streamlit change

--- text2speech/elevenlabs_streamlit.py
+++ text2speech/elevenlabs_streamlit.py
@@ -1,19 +1,40 @@
 import openai
 import streamlit as st
 import os
-from elevenlabs import generate, play, stream, set_api_key, VoiceSettings, Voice
+from elevenlabs import generate, play, stream, set_api_key, VoiceSettings, Voice, voices
 from elevenlabs.api.error import UnauthenticatedRateLimitError, RateLimitError
 
-def speak(text):
+
+def autoplay_audio(file_path: str):
+    import base64
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio controls autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
+
+def speak(text, voice):
     try:
         audio = generate(text=text, 
                          voice=Voice(
-                                voice_id='Ln9EThYHmy8oV6bISBUw',
+                                voice_id=voice,
                                 settings=VoiceSettings(stability=0.71, similarity_boost=1, style=0.0, use_speaker_boost=True)
                             ),
-                         model='eleven_multilingual_v2',
+                         model='eleven_multilingual_v2'
                          )
-        st.audio(data=audio)
+        # byteをファイルに書き込む
+        audio_path = "audio.mp3"
+        with open(audio_path, mode="wb") as f:
+            f.write(audio) # type: ignore
+
+        autoplay_audio(audio_path)
     except UnauthenticatedRateLimitError:
         e = UnauthenticatedRateLimitError("Unauthenticated Rate Limit Error")
         st.exception(e)
@@ -30,6 +51,24 @@ if elevenlabs_api_key:
     set_api_key(elevenlabs_api_key)
 else :
     st.error("ELEVENLABS_API_KEY is not set")
+
+# ここどう書くのがいいんだろ
+voice_list = voices()
+voice_id_list = [voice.voice_id for voice in voice_list]
+voice_name_list = [voice.name for voice in voice_list]
+marin_index = voice_name_list.index("marin")
+selected_voice_name = st.selectbox("Select voice", options=voice_name_list, index=marin_index)
+selected_voice_id = voice_id_list[voice_name_list.index(selected_voice_name)]
+
+with st.expander("Click to expand and enter system prompt"):
+    system_prompt = st.text_area("Enter system prompt", value=f"""あなたの名前は {selected_voice_name} です""")
+    
+    embeddings_group_name = st.text_input("Enter embeddings group name(optional)", value="")
+    if embeddings_group_name:
+        system_prompt += """
+# You can use below knowledge to respond to User's statement
+{{EMBEDDINGS_CONTEXT}}
+"""
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -50,19 +89,27 @@ if prompt:
         full_response = ""
         for response in openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
+            query=prompt,
+            groupName = embeddings_group_name,
+            headers = {
+                "Content-Type": "application/json",
+                "LangCore-Embeddings": "on",
+                "LangCore-Embeddings-Match-Threshold": "0.8",
+                "LangCore-Embeddings-Match-Count": "3",
+            } ,
             messages= [
                 {
                     "role": "system",
-                    "content": "assistantはなるべく短く20文字以内で回答してください。長い回答になる場合は、無理やり短くしてください。"
+                    "content": system_prompt
                 },
                 *st.session_state.messages
                 ],
                 stream=True,
         ):
-            full_response += response.choices[0].delta.get("content", "")
+            full_response += response.choices[0].delta.get("content", "") # type: ignore
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
 
     st.session_state.messages.append({"role": "assistant", "content": full_response})
     with st.spinner('Generating audio...'):
-        speak(full_response)
+        speak(full_response, selected_voice_id)
