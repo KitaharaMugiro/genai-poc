8819042b6ac796c0379226a64811648adfb503fb
KitaharaMugiro <ngthornym@yahoo.co.jp>
2023-09-28 09:10:41 +0900

    pocアップデート

--- web-browsing/mail-templator.py
+++ web-browsing/mail-templator.py
@@ -1,20 +1,44 @@
 import openai
 import streamlit as st
+from streamlit_feedback import streamlit_feedback
 
 openai.api_base = "https://oai.langcore.org/v1"
 
-def main():
-    st.title('メール自動作成ツール')
-    st.write('URLを入力すると、その内容を元にChatGPTを使ってメールを自動作成するツールです。')
+if "mail" not in st.session_state:
+    st.session_state["mail"] = None
+if "prompt" not in st.session_state:
+    st.session_state["prompt"] = None
+if "request_body" not in st.session_state:
+    st.session_state["request_body"] = None
+if "response_body" not in st.session_state:
+    st.session_state["response_body"] = None
 
-    url = st.text_input('URL', "https://about.yahoo.co.jp/hr/job-info/role/1601/")
-    mail_template = st.text_area('メールのテンプレート', get_mail_template(), height=500)
+def main():
+    st.title('メール自動作成デモ')
+    st.write('URLを入力すると、その内容を元にChatGPTを使ってメール文面を自動作成するデモ')
+
+    url = st.text_input('参照先URL', "https://about.yahoo.co.jp/hr/job-info/role/1601/")
+    mail_template = st.text_area('作成するメールのテンプレート', get_mail_template(), height=500)
 
     if st.button('メールを作成する'):
         with st.spinner('メールを作成中です...'):
-            mail, prompt = create_mail(url, mail_template)
+            create_mail(url, mail_template)
+    
+    if st.session_state["mail"] != None and st.session_state["prompt"] != None:
+        mail = st.session_state["mail"]
+        prompt = st.session_state["prompt"]
+        request_body = st.session_state["request_body"]
+        response_body = st.session_state["response_body"]
+
         st.markdown('<span style="font-size:0.8em;color:gray">メールを作成しました！</span>', unsafe_allow_html=True)
         st.text_area("作成されたメール", mail, height=500)
+
+        streamlit_feedback(
+            feedback_type="thumbs", 
+            optional_text_label="フィードバックをお願いします",
+            on_submit=on_submit,
+            args=[request_body, response_body, st.secrets["OPENAI_API_KEY"]]
+            )
 
         expander = st.expander("実行したプロンプト", expanded=False)
         with expander:
@@ -54,19 +78,23 @@ def create_mail(url, mail_template):
 
     補完したMAIL_TEMPLATE:
     """
-
-    res = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        temperature=0,
-        messages=[
+    request_body = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
             {
                 "role": "system",
                 "content": prompt
             },
-        ]
-
-    )
+        ],
+        "user": "山田太郎",
+    }
+    res = openai.ChatCompletion.create(**request_body)
     mail = res.choices[0].message.content
+
+    st.session_state["request_body"] = request_body
+    st.session_state["response_body"] = res
+    st.session_state["mail"] = mail
+    st.session_state["prompt"] = prompt
 
     return mail, prompt
 
@@ -77,7 +105,7 @@ def get_mail_template():
     MAIL_TEMPLATE = f"""
 [企業名]様
 
-初めまして、ちぇんと申します。
+初めまして、田中太郎と申します。
 
 ホームページを拝見し、[企業の困っていること]で課題を抱えられているのではないかと思い、ご連絡させていただきました。
 
@@ -110,6 +138,35 @@ def get_jikoku():
     day2 = day2.strftime('%-m/%-d')
     day3 = day3.strftime('%-m/%-d')
     return day1, day2, day3, day1_youbi, day2_youbi, day3_youbi
+
+def on_submit(feedback, request_body, response_body, openai_api_key):
+    import requests
+    import json
+    feedback_type = feedback["type"]
+    score = feedback["score"]
+    if score == "👍":
+        score = 1
+    elif score == "👎":
+        score = 0
+    optional_text_label = feedback["text"]
+
+    url = "http://langcore.org/api/feedback"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + openai_api_key
+    }
+    data = {
+        "request_body": request_body,
+        "response_body": response_body,
+        "feedback_type": feedback_type,
+        "score": score,
+        "optional_text_label": optional_text_label
+    }
+    requests.post(url, headers=headers, data=json.dumps(data))
+    st.toast("フィードバックを送信しました。")
+    # URLを表示する
+    st.write("フィードバックはこちらに記録されます: https://langcore.org/feedback")
+
 
 if __name__ == '__main__':
     main()
