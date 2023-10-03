3c5725fb9bcdbc5144b00531b2c52df03614c680
KitaharaMugiro <ngthornym@yahoo.co.jp>
2023-10-03 18:24:57 +0900

    commit

--- rate-limit/app.py
+++ rate-limit/app.py
@@ -10,11 +10,10 @@ def main():
     st.text("今回は1分間あたり1回までの利用制限を加えています。")
     user_input = st.text_input("キャッチコピーを作成するためのキーワードを入力してください: ")
     if st.button("キャッチコピーを生成"):
-        catchphrase = call_chatgpt_api(user_input)
-        st.write("生成されたキャッチコピー: ", catchphrase)
+        create_catchcopy(user_input)
 
 
-def call_chatgpt_api(input_text):
+def create_catchcopy(input_text):
     try:
         prompt = f"""次のお題からキャッチコピーを作成してください。
 お題: {input_text}
@@ -25,9 +24,9 @@ def call_chatgpt_api(input_text):
             temperature=0.7,
             max_tokens=100,
         )
-        return response.choices[0].text.strip()
+        st.write("生成されたキャッチコピー: " + response.choices[0].text.strip())
     except Exception as e:
-        return f"エラー: キャッチコピーを生成できませんでした。{str(e)}"
+        st.error("エラーが発生しました。 " + str(e))
 
 
 if __name__ == "__main__":
