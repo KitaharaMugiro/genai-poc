28d504117fad245e4a03c74f847f4117b21d66ad
KitaharaMugiro <ngthornym@yahoo.co.jp>
2023-10-13 21:59:37 +0900

    deploy

--- scraping/app.py
+++ scraping/app.py
@@ -3,29 +3,76 @@ from xml.etree import ElementTree
 import streamlit as st
 from trafilatura import fetch_url, extract
 from trafilatura.settings import use_config
+import openai
 
 
 def main():
     st.title("パレオスクレイピング")
     st.write("まずブログポストのURLの一覧を取得します")
 
+    start_number = st.number_input("開始番号", value=0)
+    count_number = st.number_input("取得件数", value=100)
+
     if st.button("一覧取得"):
         sitemap_index = "https://yuchrszk.blogspot.com/sitemap.xml"
         urls = fetch_sitemap_urls(sitemap_index)
-        # data = save_urls_to_file(urls, "sitemap_urls.txt")
-
-        st.download_button(
-            "ダウンロードする", urls_to_string(urls), file_name="sitemap_urls.txt"
-        )
 
         if urls:
-            st.write("最初の10件のテキストを取得して表示します。")
+            st.write(f"{start_number}からのポストを{count_number}件のテキストを取得して表示します。")
             contents = []
-            for url in urls[:10]:
+            for url in urls[start_number : start_number + count_number]:
                 content = fetch_contents(url)
-                contents.append({"url": url, "content": content})
+                summarized_content = summarize(content)
+                contents.append(
+                    {"url": url, "content": summarized_content, "is_summarized": True}
+                )
+                embed_text_with_openai(
+                    st.secrets["OPENAI_API_KEY"],
+                    summarized_content,
+                    url,
+                    groupName="parao",
+                )
 
-            st.json(contents)
+
+def embed_text_with_openai(api_key, text, metadata_input, groupName="default"):
+    url = "http://langcore.org/api/embeddings"
+    headers = {"Authorization": f"Bearer {api_key}", "Content-Type": "application/json"}
+    data = {"input": text, "groupName": groupName, "metadata": metadata_input}
+
+    response = requests.post(url, json=data, headers=headers)
+
+    if response.status_code != 200:
+        st.error(f"Error {response.status_code}: {response.text}")
+        return None
+
+    return response.json()
+
+
+def summarize(content: str) -> str:
+    content = content[:3000]
+    prompt = f"""貴方は文章分析の専門家として提供されたウェブページの内容を人にわかりやすく伝わるように100文字程度で簡潔に要約してください。
+ウェブページの内容は ``` で括って提供されます。
+重要なポイントを抑えて要約してください。
+ウェブページの本文が長い場合は途中で切られて提供されます。
+出力は日本語でお願いします。
+
+ウェブページ
+```
+{content}
+```
+
+では、日本語でよろしくお願いします
+    """
+
+    res = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        temperature=0,
+        messages=[
+            {"role": "system", "content": prompt},
+        ],
+    )
+    summarized_text = res.choices[0].message.content  # type: ignore
+    return summarized_text
 
 
 def fetch_contents(url: str) -> str:
@@ -63,12 +110,13 @@ def fetch_sitemap_urls(sitemap_url: str) -> list:
     return entry_urls
 
 
-def save_urls_to_file(urls: list, filename: str) -> None:
-    """URLのリストを指定したファイルに保存する関数"""
-    with open(filename, "w") as file:
-        for url in urls:
-            file.write(url + "\n")
-
-
 if __name__ == "__main__":
-    main()
+    ## login ##
+    password = st.text_input("Password", type="password")
+    if password == "":
+        pass
+    elif password != st.secrets["PASSWORD"]:
+        st.error("the password you entered is incorrect")
+        st.stop()
+    else:
+        main()
---
+++ scraping/pages/chat.py
@@ -0,0 +1,66 @@
+import openai
+import streamlit as st
+
+
+st.title("Parao Chatbot")
+# APIキーの入力
+api_key = st.secrets["OPENAI_API_KEY"]
+# グループ名の入力
+group_name = "parao"
+
+openai.api_base = "https://oai.langcore.org/v1"
+openai.api_key = api_key
+
+with st.expander("Click to expand and enter system prompt"):
+    system_prompt = st.text_area(
+        "Enter system prompt",
+        value="""
+Answer questions as truthfully as possible, and ONLY answer the questions using the information from search result, do not speculate or your own knowledge.
+You must attach the URL of the search result to the answer.
+You must answer the question in Japanese.
+
+[Search Result]
+{{EMBEDDINGS_CONTEXT}}""",
+    )
+
+    match_threshold = st.text_input("Embeddings-Match-Threshhold", value="0.5")
+    match_count = st.text_input("Embeddings-Match-Count", value="3")
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+prompt = st.chat_input("あなたの心に抱えていることを相談してください")
+
+if prompt:
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            query=prompt,
+            groupName=group_name,
+            headers={
+                "Content-Type": "application/json",
+                "LangCore-Embeddings": "on",
+                "LangCore-Embeddings-Match-Threshold": match_threshold,
+                "LangCore-Embeddings-Match-Count": match_count,
+            },
+            messages=[
+                {"role": "system", "content": system_prompt},
+                *st.session_state.messages,
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")  # type: ignore
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
